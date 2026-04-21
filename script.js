0c3b8e8f6934dff6159e6deeec74eb24e32875cb
asuaisha <aishairegbulem@asu.edu>
2026-04-21 00:03:14 -0700

    Add hero section with Vanta fog background

---
+++ script.js
@@ -0,0 +1,13 @@
+// add to your script.js
+VANTA.FOG({
+    el: "#hero",
+  mouseControls: true,
+  touchControls: true,
+  highlightColor: 0xff2d78,
+  midtoneColor: 0x990033,
+  lowlightColor: 0x080808,
+  baseColor: 0x000000,
+  blurFactor: 0.7,
+  speed: 0.3,
+  zoom: 0.1
+  })
